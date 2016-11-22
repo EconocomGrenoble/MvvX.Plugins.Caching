609bb7c7db1ba4cf2aaacc158093065b26213aa5
MACK Mathieu <mathieu.mack@LIVE.FR>
2016-11-22 13:27:30 +0100

    Adding support of Android

---
+++ MvvX.Plugins.Caching.Droid/System.Runtime.Caching/CacheEntryChangeMonitor.cs
@@ -0,0 +1,10 @@
+
+using System.Collections.Generic;
+
+namespace MvvX.Plugins.Caching.Droid.System.Runtime.Caching
+{
+    internal class CacheEntryChangeMonitor
+    {
+        public IList<string> DependencyKeys { get; set; }
+    }
+}
