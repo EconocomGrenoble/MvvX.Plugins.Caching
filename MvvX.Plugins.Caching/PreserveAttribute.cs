0b542ed690f2e2ceb18f8733e39a8a94a7a9e888
MACK Mathieu <mathieu.mack@econocom.com>
2019-09-27 14:36:15 +0200

    Update preserve attribute

--- MvvX.Plugins.Caching/PreserveAttribute.cs
+++ MvvX.Plugins.Caching/PreserveAttribute.cs
@@ -7,6 +7,6 @@ namespace MvvX.Plugins.Caching
     {
         public PreserveAttribute() { }
         public bool AllMembers { get; set; }
-        public bool Conditional { get; set; }
+        public bool Conditional { get; set; } 
     }
 }
